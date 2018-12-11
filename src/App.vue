113ca0c5d8044ca0f5b62f87854de04b3cda41b1
Joseph Podwys <jpodwys@josephpodwysmbp.lan>
2018-12-10 17:43:57 -0700

    Add component-level list filtering

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
     <h1>Star Wars API Demo</h1>
-    <button class="inline" @click="getPeople">Get People</button>
-    <button class="inline" @click="getPlanets">Get Starships</button>
-    <button class="inline" @click="getStarships">Get Starships</button>
-    <div v-if="loading">Loading...</div>
-    <ul v-if="!loading">
-      <li v-for="item in list" :key="item.name">
+
+    <div class="spacing">
+      <button class="inline" @click="getPeople">Get People</button>
+      <button class="inline" @click="getPlanets">Get Planets</button>
+      <button class="inline" @click="getStarships">Get Starships</button>
+    </div>
+
+    <div class="spacing">
+      <input placeholder="Filter" v-model="filterText"/>
+    </div>
+
+    <div class="spacing" v-if="loading">Loading...</div>
+
+    <ul class="spacing" v-if="!loading">
+      <li v-for="item in filteredList" :key="item.name">
         {{ item.name }}
       </li>
     </ul>
@@ -18,7 +27,17 @@ import { mapState, mapActions } from 'vuex'
 
 export default {
   name: 'app',
-  computed:  mapState([ 'loading', 'list' ]),
+  data() {
+    return { filterText: '' }
+  },
+  computed:  {
+    filteredList: function() {
+      return this.list.filter(item =>
+        ~item.name.toLowerCase().indexOf(this.filterText.toLowerCase())
+      );
+    },
+    ...mapState([ 'loading', 'list' ])
+  },
   methods: mapActions([ 'getPeople', 'getPlanets', 'getStarships' ])
 }
 </script>
@@ -26,5 +45,9 @@ export default {
 <style scoped>
   .inline {
     display: inline-block;
+  }
+  .spacing {
+    padding: 10px 0;
+    margin: 0;
   }
 </style>
